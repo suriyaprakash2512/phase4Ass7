86e3785e44b2990721f65796780596140bc21542
suriyaprakash2512 <suryasnra@gmail.com>
2024-01-15 17:21:48 +0530

    azure

---
+++ WebAppAzureAss/WebAppAzureAss/Controllers/PlayersController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Numerics;
+using WebAppAzureAss.Models;
+
+namespace WebAppAzureAss.Controllers
+{
+    public class PlayersController : Controller
+    {
+        private static List<Players> players = new List<Players>
+    {
+        new Players { PId = 1, PName = "Kane Williamson", PCountry = "New Zealand", PType = "Batsman" },
+        new Players { PId = 2, PName = "Jadeja", PCountry = "India", PType = "All-Rounder" },
+        new Players { PId = 3,PName="Ben Stokes",PCountry="England",PType="All-Rounder"}
+
+    };
+        public IActionResult Index()
+        {
+            return View(players);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View(new Players());
+        }
+        [HttpPost]
+        [HttpPost]
+        public IActionResult Create(Players pla)
+        {
+            if (ModelState.IsValid)
+            {
+                players.Add(pla);
+            }
+            return View();
+        }
+
+    }
+}
---
+++ WebAppAzureAss/WebAppAzureAss/Models/Players.cs
@@ -0,0 +1,11 @@
+﻿namespace WebAppAzureAss.Models
+{
+    public class Players
+    {
+        public int PId { get; set; }
+        public string? PName { get; set; }
+        public string? PCountry { get; set; }
+        public string? PType { get; set; }
+
+    }
+}
